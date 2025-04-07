83cf69f16f7115ef1b79d788c19aa2c43987f40c
karabinn <krabin.dev@gmail.com>
2025-04-07 18:13:15 +0200

    Fix suppression d'image et gestion des erreurs console

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,4 +1,6 @@
 const Book = require('../models/Book');
+const fs = require('fs');
+const path = require('path');
 
 // Créer un livre
 exports.createBook = async (req, res) => {
@@ -40,16 +42,58 @@ exports.createBook = async (req, res) => {
 
 // Modifier un livre
 exports.modifyBook = (req, res) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Livre modifié !' }))
-    .catch((error) => res.status(400).json({ error }));
+  console.log('Modification du livre avec ID:', req.params.id);
+
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+        if (!book) {
+        return res.status(404).json({ message: 'Livre non trouvé' });
+        }
+        if (book.userId !== req.auth.userId) {
+        return res.status(403).json({ message: 'Requête non autorisée' });
+        }
+    // Supprimer _id s'il est présent dans req.body
+    delete req.body._id;
+    
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => {
+        console.log('Livre modifié');
+        res.status(200).json({ message: 'Livre modifié !' });
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la mise à jour:', error);
+        res.status(400).json({ error });
+      });
+    })
+    .catch((error) => {
+        console.error('Erreur serveur lors de la recherche du livre:', error);
+        res.status(500).json({ error });
+    });
 };
 
 // Supprimer un livre
 exports.deleteBook = (req, res) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+        if (!book) {
+            return res.status(404).json({ message: 'Livre non trouvé' });
+        }
+        if (book.userId !== req.auth.userId) {
+        return res.status(403).json({ message: 'Requête non autorisée'});
+        }
+        const filename = book.imageUrl.split('/images/')[1];
+
+        fs.unlink(path.join('images', filename), (err) => {
+          if (err) {
+            console.error('Erreur lors de la suppression de l’image :', err);
+            return res.status(500).json({ message: 'Erreur lors de la suppression du fichier' });
+          }    
+
   Book.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Livre supprimé !' }))
     .catch((error) => res.status(400).json({ error }));
+    })
+  });
 };
 
 // Récupérer un seul livre
@@ -72,4 +116,32 @@ exports.getBestRatedBooks = (req, res) => {
     .limit(3) // top 3
     .then(books => res.status(200).json(books))
     .catch(error => res.status(400).json({ error }));
+};
+
+exports.rateBook = (req,res) => {
+    const { userId, rating } = req.body;
+    if (rating < 0 || rating > 5) {
+        return res.status(400).json({ error: 'La note doit être en 0 et 5.' });
+    }
+
+    Book.findOne({ _id: req.params.id})
+    .then(book => {
+        const alreadyRated = book.ratings.some((rating) => rating.userId.toString() === userId);
+    if (alreadyRated) {
+          return res.status(403).json({ message: 'Vous avez déjà noté ce livre.' });
+    }
+        
+    // Ajouter la note dans le tableau "ratings"
+    book.ratings.push({ userId, grade: rating });
+
+    // Calculer la nouvelle note moyenne
+    const totalRatings = book.ratings.reduce((sum, rating) => sum + rating.grade, 0);
+    book.averageRating = totalRatings / book.ratings.length;
+
+    // Sauvegarder le livre mis à jour
+    book.save()
+    .then(() => res.status(200).json(book))
+    .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(404).json({ message: 'Livre non trouvé.', error }));
 };
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -12,7 +12,8 @@ router.get('/bestrating', bookCtrl.getBestRatedBooks);
 router.get('/', bookCtrl.getAllBooks);
 router.post('/', auth, multer, bookCtrl.createBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', bookCtrl.modifyBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
 
 module.exports = router;
